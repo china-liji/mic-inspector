198af621bee0e1c7216c3d47af640069e4c0cf04
Michael_Lee <hello_zaizi@163.com>
2020-08-11 14:40:50 +0800

    insert the index name to front

--- test/index.tsx
+++ test/index.tsx
@@ -38,7 +38,7 @@ export function Test(): React.ReactElement {
         <h4>Property</h4>
         <Property name='object' value={obj} defaultExpand isNonenumerable={false} />
         <Property name='array' value={'123'.repeat(10).split('')} />
-        <Property name='obj' value={{ '+': 1, 0: 0, 'a': 'a' }} />
+        <Property name='obj' value={{ '+': 1, 0: 0, 'a': 'a', '-1': 99 }} />
       </blockquote>
       <blockquote>
         <h4>PropertyName</h4>
