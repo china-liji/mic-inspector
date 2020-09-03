d5af79cd9e087d7461c80343cd12bb5cdd14eadc
Michael_Lee <hello_zaizi@163.com>
2020-09-03 18:57:09 +0800

    concat the arraylike method names

--- src/object-value/locale.tsx
+++ src/object-value/locale.tsx
@@ -9,7 +9,7 @@ import { isObjectOrFunction } from '../property-value/locale';
 
 export const { Object, Node } = window;
 
-export const arraylikeMethodNames = [...(!window.Symbol ? [] : [window.Symbol]), 'entries', 'forEach', 'item', 'keys', 'split', 'values'];
+export const arraylikeMethodNames = [...(!window.Symbol ? [] : [Symbol.iterator]), 'entries', 'forEach', 'item', 'keys', 'split', 'values'];
 
 /**
  * Returns a string represents object name
